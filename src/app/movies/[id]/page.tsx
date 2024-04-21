b22cc07f8976578149b438f8f6ba052358fe7617
GeorgeU2030 <georgeu2021@gmail.com>
2024-04-21 16:47:07 -0500

    feat: page_one_movie_styles_and_section

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect,useState } from "react";
 import { useParams} from "next/navigation";
 import Cookies from "js-cookie";
+import {Button} from "@/components/ui/button";
 
 export default function MovieId(){
 
@@ -19,6 +20,7 @@ export default function MovieId(){
 
     const token = Cookies.get('token');
     const {id} = useParams()
+    const [votation, setVotation] = useState<string>('')
     const [movie, setMovie] = useState<Movie | null>(null)
 
     useEffect (() => {
@@ -39,19 +41,38 @@ export default function MovieId(){
     },[])
 
 
+    function votationCritic (){
+        setVotation('critic')
+    }
+
+    function votationOverall (){
+        setVotation('overall')
+    }
 
     return (
         <div className="min-h-screen w-screen bg-black">
-            <div className="flex w-full">
+            <div className="md:flex md:w-full md:items-center md:justify-center min-h-screen ">
                 <section className="lg:w-1/2 md:w-1/2 w-full text-white flex flex-col justify-center items-center">
-                    <img src={movie?.cover} alt={movie?.name} className="w-1/3 h-1/2"/>
+                    <img src={movie?.cover} alt={movie?.name} className="w-1/3 h-1/2 mt-8 md:mt-1"/>
                     <h1 className="text-3xl mt-4">{movie?.name}</h1>
-                    <p className="mt-2">{movie?.genre}</p>
-                    <p className="mt-1">{movie?.year}</p>
-                    <p>{movie?.duration} minutes</p>
-                    <p>{movie?.points}</p>
-                    <p className="text-sm">{movie?.amount} votes</p>
-                    {movie && <img
+                    <div className={'flex items-center justify-center mt-2'}>
+                    <p>{movie?.genre}</p>
+                    <p className={'ml-1'}> - {movie?.year}</p>
+                    </div>
+                    <p className={'mt-1'}>{movie?.duration} minutes</p>
+                    <div className={'flex items-center justify-center mt-2'}>
+                    {movie && movie.amount == 0 &&
+                        <>
+                        <img
+                        src="/snowfault.png"
+                        className={"w-16 h-16"}
+                    />
+                        <p className={'font-semibold ml-2'}>----</p>
+                        </>
+                    }
+
+                    {movie && movie.amount > 0 && <>
+                        <img
                         src={
                             movie.points < 30 ? "/snow30.png" :
                                 (movie.points >= 30 && movie.points < 50) ? "/snow50.png" :
@@ -62,14 +83,48 @@ export default function MovieId(){
                         }
                         className="w-16 h-16"
                         alt="image"
-                    />}
+                        />
+                        <p>{movie.points}</p>
+                        </>
+                    }
+                    </div>
+                    <p className="text-sm">{movie?.amount} votes</p>
                     {movie?.oscars === 1 &&
-                        <span className="text-center text-yellow-600 font-semibold"> {movie.oscars} Oscar Award </span>}
-                    {movie && movie?.oscars > 1 && <span
-                        className="text-center text-yellow-600 font-semibold">  {movie?.oscars} Oscar Awards </span>}
+                        <div className={'flex justify-center items-center'}>
+                        <span className="text-center text-yellow-600 font-semibold"> {movie.oscars} Oscar Award </span>
+                        <img src={'/oscar.png'} className={'w-12 h-12'}/>
+                        </div>
+                        }
+                    {movie && movie?.oscars > 1 &&
+                        <div className={'flex justify-center items-center'}>
+                        <span
+                        className="text-center text-yellow-600 font-semibold">  {movie?.oscars} Oscar Awards </span>
+                        <img src={'/oscar.png'} className={'w-12 h-12'}/>
+                        </div>
+                        }
                 </section>
-                <section className="lg:w-1/2 md:w-1/2 w-full text-white flex flex-col">
+                <section className="lg:w-1/2 md:w-1/2 w-full text-white flex flex-col items-center">
+                        <div className={'flex flex-col justify-center items-center w-full mb-6'}>
+                            <img src={'/snowlogo.png'} className={'w-12 h-12'}/>
+                            <h1 className={'ml-2 text-xl font-semibold'}>Snow</h1>
+                            <h2 className={'mt-2'}>Vote the Movie </h2>
 
+                            <p className={'w-1/2 text-sm mt-1'}>
+                                In Snow, dive into the world of cinema and have your say in shaping its destiny. Whether you are casting your overall vote or critic vote. Join us and let your cinematic passion guide the way!
+                            </p>
+
+                            <div className={'mt-3'}>
+                                <Button className={'mr-2 bg-[#2953A6] hover:bg-slate-100 hover:text-[#2953a6] font-semibold'}
+                                onClick={votationOverall}
+                                >Overall Vote</Button>
+                                <Button className={'ml-2 px-6 bg-[#2953A6] hover:bg-slate-100 hover:text-[#2953a6] font-semibold'}
+                                onClick={votationCritic}
+                                >Critic Vote</Button>
+                            </div>
+
+
+
+                        </div>
                 </section>
             </div>
         </div>
